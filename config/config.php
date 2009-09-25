c86027e4791a723e00a4e652f5fe2fc88e6c074f
Kousuke Ebihara <ebihara@tejimaya.com>
2009-09-26 05:16:52 +0900

    added plugin configuration file for registering autoloader for Symfony Templating

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * This file is part of the sfSymfonyTemplatingViewPlugin package.
+ * (c) Kousuke Ebihara (http://co3k.org/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+require_once dirname(__FILE__).'/../lib/vendor/SymfonyTemplating/sfTemplateAutoloader.php';
+sfTemplateAutoloader::register();
