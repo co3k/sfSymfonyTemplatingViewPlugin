ef0283f1ea6b3e68312e951f9b03faef0c0142f3
Kousuke Ebihara <ebihara@tejimaya.com>
2009-10-18 22:27:12 +0900

    add the sfTemplateAbstractLoader

---
+++ lib/loader/sfTemplateAbstractLoader.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the sfSymfonyTemplatingViewPlugin package.
+ * (c) Kousuke Ebihara (http://co3k.org/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+abstract class sfTemplateAbstractLoader extends sfTemplateLoader
+{
+  protected
+    $context = null,
+    $view = null,
+    $configure = array();
+
+  public function __construct(sfView $view, sfContext $context, array $configure = array())
+  {
+    $this->context = $context;
+    $this->view = $view;
+    $this->configure = $configure;
+
+    $this->configure();
+  }
+
+  public function getParameter($name, $default = null)
+  {
+    if (isset($this->configure[$name]))
+    {
+      return $this->configure[$name];
+    }
+
+    return $default;
+  }
+
+  public function configure()
+  {
+  }
+}
